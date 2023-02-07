add2f52bd727bfb56fd3c911a57f54bf63c446d3
EiriniKot <eirkotz@gmail.com>
2023-02-07 14:14:40 +0200

    documentation in training tools and some reqs

--- src/training_tools.py
+++ src/training_tools.py
@@ -5,12 +5,15 @@ from transformers import TrainerCallback
 from copy import deepcopy
 
 
-def scheduler(epoch, lr = 1e-3):
-  # Scheduler reduces lr after the first 5 epochs
-  if epoch < 100:
-    return lr
-  else:
-    return lr * tf.math.exp(-0.1)
+def scheduler(epoch, lr=1e-3):
+    """
+    Scheduler that reduces exponentialy after 100 epoch
+    """
+    # Scheduler reduces lr after the first 5 epochs
+    if epoch < 100:
+        return lr
+    else:
+        return lr * tf.math.exp(-0.1)
 
 
 def model_sequential(dict_layers={},
@@ -19,19 +22,23 @@ def model_sequential(dict_layers={},
                      metrics=[tf.keras.metrics.CategoricalAccuracy(name='cat_accuracy'),
                               tf.keras.metrics.Precision(name='prec')]):
 
-  layers=list(map(lambda i: getattr(tf.keras.layers, i[0])(**i[1]), dict_layers.items()))
-  model = tf.keras.Sequential(layers)
-  model.compile(loss=loss,
+    layers=list(map(lambda i: getattr(tf.keras.layers, i[0])(**i[1]), dict_layers.items()))
+    model = tf.keras.Sequential(layers)
+    model.compile(loss=loss,
                 optimizer=optimizer,
                 metrics=metrics)
-  return model
+    return model
 
 
 def compute_metrics(eval_pred, metrics = [("accuracy_score", {}), ("recall_score", {'average': 'macro'}),\
                                           ("f1_score", {'average': 'macro'}),\
                                           ("precision_score", {'average': 'macro'}), \
                                           ("confusion_matrix", {})]):
-
+    """
+    This function uses sklearn to calc all the metrics that has been passed into metrics argument.
+    Metrics should be a list with tuples. First index of the tuple should be the name of the metric
+    and the second a dictionary of the kwargs for this metric
+    """
     logits, labels = eval_pred
     predictions = np.argmax(logits, axis=-1)
     dict_eval = dict(map(lambda i: (i[0].split('-')[0], getattr(sklearn.metrics, i[0])(y_pred=predictions, y_true=labels, **i[1])), metrics))
